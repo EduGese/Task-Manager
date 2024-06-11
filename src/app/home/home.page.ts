1cf55bdef299f9993488e0ae7ec876897ea8e91c
EduGese <122921699+EduGese@users.noreply.github.com>
2024-06-11 18:51:55 +0200

    Update home.page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { UsersComponent } from '../components/users/users.component';
+import { TasksComponent } from '../components/tasks/tasks.component';
 
 @Component({
 selector: 'app-home',
 templateUrl: 'home.page.html',
 styleUrls: ['home.page.scss'],
 standalone: true,
-imports: [IonicModule, UsersComponent ],
+imports: [IonicModule, TasksComponent],
 })
 export class HomePage {
 constructor() {}
