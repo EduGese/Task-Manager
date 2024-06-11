4ab6ee839fabc6ab2b1896cb2d20fcadf9212134
EduGese <122921699+EduGese@users.noreply.github.com>
2024-06-11 19:02:27 +0200

    Update Storage service: Implement CRUD accordingly to the new Schema

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -3,74 +3,84 @@ import { SQLiteDBConnection } from '@capacitor-community/sqlite';
      import { SQLiteService } from './sqlite.service';
      import { DbnameVersionService } from './dbname-version.service';
      import { UserUpgradeStatements } from '../upgrades/user.upgrade.statements';
-     import { User } from '../models/user';
+     import { Task } from '../models/task';
      import { BehaviorSubject, Observable } from 'rxjs';
 
      @Injectable()
      export class StorageService {
-         public userList: BehaviorSubject<User[]> =
-                                     new BehaviorSubject<User[]>([]);
-         private databaseName: string = "";
-         private uUpdStmts: UserUpgradeStatements = new UserUpgradeStatements();
-         private versionUpgrades;
-         private loadToVersion;
-         private db!: SQLiteDBConnection;
-         private isUserReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+       public taskList: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
+       private databaseName: string = '';
+       private uUpdStmts: UserUpgradeStatements = new UserUpgradeStatements();
+       private versionUpgrades;
+       private loadToVersion;
+       private db!: SQLiteDBConnection;
+       private isTaskReady: BehaviorSubject<boolean> = new BehaviorSubject(
+         false
+       );
 
-         constructor(private sqliteService: SQLiteService,
-                     private dbVerService: DbnameVersionService) {
-             this.versionUpgrades = this.uUpdStmts.userUpgrades;
-             this.loadToVersion = this.versionUpgrades[this.versionUpgrades.length-1].toVersion;
-         }
-         async initializeDatabase(dbName: string) {
-             this.databaseName = dbName;
-             // create upgrade statements
-             await this.sqliteService
-             .addUpgradeStatement({  database: this.databaseName,
-                                     upgrade: this.versionUpgrades});
-             // create and/or open the database
-             this.db = await this.sqliteService.openDatabase(
-                                                 this.databaseName,
-                                                 false,
-                                                 'no-encryption',
-                                                 this.loadToVersion,
-                                                 false
-             );
-             this.dbVerService.set(this.databaseName,this.loadToVersion);
+       constructor(
+         private sqliteService: SQLiteService,
+         private dbVerService: DbnameVersionService
+       ) {
+         this.versionUpgrades = this.uUpdStmts.userUpgrades;
+         this.loadToVersion =
+           this.versionUpgrades[this.versionUpgrades.length - 1].toVersion;
+       }
+       async initializeDatabase(dbName: string) {
+         this.databaseName = dbName;
+         // create upgrade statements
+         await this.sqliteService.addUpgradeStatement({
+           database: this.databaseName,
+           upgrade: this.versionUpgrades,
+         });
+         // create and/or open the database
+         this.db = await this.sqliteService.openDatabase(
+           this.databaseName,
+           false,
+           'no-encryption',
+           this.loadToVersion,
+           false
+         );
+         this.dbVerService.set(this.databaseName, this.loadToVersion);
 
-             await this.getUsers();
-         }
-         // Current database state
-         userState() {
-             return this.isUserReady.asObservable();
-         }
-         fetchUsers(): Observable<User[]> {
-             return this.userList.asObservable();
-         }
+         await this.getTasks();
+       }
+       // Current database state
+       taskState() {
+         return this.isTaskReady.asObservable();
+       }
+ 
+       fetchTasks(): Observable<Task[]> {
+         return this.taskList.asObservable();
+       }
 
-         async loadUsers() {
-             const users: User[]= (await this.db.query('SELECT * FROM users;')).values as User[];
-             this.userList.next(users);
-         }
-         // CRUD Operations
-         async getUsers() {
-             await this.loadUsers();
-             this.isUserReady.next(true);
-         }
-         async addUser(name: string) {
-             const sql = `INSERT INTO users (name) VALUES (?);`;
-             await this.db.run(sql,[name]);
-             await this.getUsers();
-         }
+       async loadTasks() {
+         const tasks: Task[] = (await this.db.query('SELECT * FROM tasks;')).values as Task[];
+         this.taskList.next(tasks);
+       }
+       // CRUD Operations
 
-         async updateUserById(id: string, active: number) {
-             const sql = `UPDATE users SET active=${active} WHERE id=${id}`;
-             await this.db.run(sql);
-             await this.getUsers();
-         }
-         async deleteUserById(id: string) {
-             const sql = `DELETE FROM users WHERE id=${id}`;
-             await this.db.run(sql);
-             await this.getUsers();
-         }
+       async getTasks() {
+         await this.loadTasks();
+         this.isTaskReady.next(true);
+       }
+ 
+        async addTask(name: string, description: string, priority: string, tag: string) {
+            const sql = `INSERT INTO tasks (name, description, priority, tag) VALUES (?, ?, ?, ?);`;
+            await this.db.run(sql, [name, description, priority, tag]);
+            await this.getTasks();
+        }
+
+
+        async updateTaskById(id: string, name: string, description: string, priority: string, tag: string) {
+            const sql = `UPDATE tasks SET name=${name}, description=${description}, priority=${priority}, tag=${tag} WHERE id=${id}`;
+            await this.db.run(sql);
+            await this.getTasks();
+        }
+
+        async deleteTaskById(id: string) {
+            const sql = `DELETE FROM tasks WHERE id=${id}`;
+            await this.db.run(sql);
+            await this.getTasks();
+            }
      }
