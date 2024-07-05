f05a1e6f20a8bd90fa75dcf6497a103e3d9e4372
EduGese <122921699+EduGese@users.noreply.github.com>
2024-07-05 13:04:09 +0200

    Update task interface

--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -6,6 +6,8 @@ export interface Task {
     tag: string,
     done: number, // 0 or 1,
     creation_date:string,
-    due_date: string
+    due_date: string,
+    notification_date_range: string,
+    notification_date: string
     
 }
