a97389df35ff190f990e3a1366559efcb1393780
EduGese <122921699+EduGese@users.noreply.github.com>
2024-07-05 12:52:53 +0200

    Feature: Delete all completed tasks
    - New deleteCompletedTasks function which querie upon ddbb
    - Implement actionSheet
    - New deleteAllcompletedTasks function that call storage service function

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -8,7 +8,9 @@ import { SQLiteDBConnection } from '@capacitor-community/sqlite';
 
      @Injectable()
      export class StorageService {
-       public taskList: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
+       public taskList: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>(
+         []
+       );
        private databaseName: string = '';
        private tUpdStmts: TaskUpgradeStatements = new TaskUpgradeStatements();
        private versionUpgrades;
@@ -49,13 +51,14 @@ import { SQLiteDBConnection } from '@capacitor-community/sqlite';
        taskState() {
          return this.isTaskReady.asObservable();
        }
- 
+
        fetchTasks(): Observable<Task[]> {
          return this.taskList.asObservable();
        }
 
        async loadTasks() {
-         const tasks: Task[] = (await this.db.query('SELECT * FROM tasks;')).values as Task[];
+         const tasks: Task[] = (await this.db.query('SELECT * FROM tasks;'))
+           .values as Task[];
          this.taskList.next(tasks);
        }
        // CRUD Operations
@@ -64,40 +67,65 @@ import { SQLiteDBConnection } from '@capacitor-community/sqlite';
          await this.loadTasks();
          this.isTaskReady.next(true);
        }
- 
-        async addTask(name: string, description: string, priority: string, tag: string, creation_date:string, due_date: string) {
-            const sql = `INSERT INTO tasks (name, description, priority, tag, creation_date, due_date) VALUES (?, ?, ?, ?, ?, ?);`;
-            await this.db.run(sql, [name, description, priority, tag, creation_date, due_date]);
-            await this.getTasks();
-        }
 
+       async addTask(
+         name: string,
+         description: string,
+         priority: string,
+         tag: string,
+         creation_date: string,
+         due_date: string
+       ) {
+         const sql = `INSERT INTO tasks (name, description, priority, tag, creation_date, due_date) VALUES (?, ?, ?, ?, ?, ?);`;
+         await this.db.run(sql, [
+           name,
+           description,
+           priority,
+           tag,
+           creation_date,
+           due_date,
+         ]);
+         await this.getTasks();
+       }
 
-        async updateTaskById(id: string, name: string, description: string, priority: string, tag: string, due_date: string) {
-          try {
-            const sql = `UPDATE tasks SET name = ?, description = ?, priority = ?, tag = ?, due_date = ? WHERE id = ?`;
-            const params = [name, description, priority, tag, due_date,  id];
-            await this.db.run(sql, params);
-            await this.getTasks();
-          } catch (error) {
-            console.error('Error updating task:', error);
-          }
-        }
+       async updateTaskById(
+         id: string,
+         name: string,
+         description: string,
+         priority: string,
+         tag: string,
+         due_date: string
+       ) {
+         try {
+           const sql = `UPDATE tasks SET name = ?, description = ?, priority = ?, tag = ?, due_date = ? WHERE id = ?`;
+           const params = [name, description, priority, tag, due_date, id];
+           await this.db.run(sql, params);
+           await this.getTasks();
+         } catch (error) {
+           console.error('Error updating task:', error);
+         }
+       }
 
-        async deleteTaskById(id: string) {
-            const sql = `DELETE FROM tasks WHERE id=${id}`;
-            await this.db.run(sql);
-            await this.getTasks();
-            }
-        
-        async updateTaskStatusById(id: string, done: boolean) {
-          let status;
-          if(done) {
-            status = 1;
-          } else {
-            status = 0;
-          }
-            const sql = `UPDATE tasks SET done=${status} WHERE id=${id}`;
-            await this.db.run(sql);
-            await this.getTasks();
-        }
+       async deleteTaskById(id: string) {
+         const sql = `DELETE FROM tasks WHERE id=${id}`;
+         await this.db.run(sql);
+         await this.getTasks();
+       }
+       async deleteCompletedTasks() {
+         const sql = `DELETE FROM tasks WHERE done=${1}`;
+         await this.db.run(sql);
+         await this.getTasks();
+       }
+
+       async updateTaskStatusById(id: string, done: boolean) {
+         let status;
+         if (done) {
+           status = 1;
+         } else {
+           status = 0;
+         }
+         const sql = `UPDATE tasks SET done=${status} WHERE id=${id}`;
+         await this.db.run(sql);
+         await this.getTasks();
+       }
      }
